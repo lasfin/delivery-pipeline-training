438f28a0bd6a72c139f0d0e00dd43735b35f64d4
lasfin <evgenij-kopica@yandex.ru>
2016-04-19 16:49:22 +0300

    code style changes

--- src/app.js
+++ src/app.js
@@ -1,29 +1,29 @@
 angular.module('crud', [])
-.controller('CrudController', ["$scope", "$http", function($scope, $http) {
+    .controller('CrudController', ["$scope", "$http", function($scope, $http){
 
-  $scope.data = '{"foo" : "some value"}';
+        $scope.data = '{"foo" : "some value"}';
 
-  var get = function(){
-    $http({method: "GET", url: "/items"})
-    .then(function(res){ 
-      $scope.items = res.data;
-    });
-  }
+        var get = function(){
+            $http({method: "GET", url: "/items"})
+                .then(function(res){
+                    $scope.items = res.data;
+                });
+        };
 
-  get();
+        get();
 
-  $scope.clear = function () {
-    $http({method: "delete", url: "/items"})
-    .then(function(res){ 
-      get();
-    })
-  };
-  
-  $scope.add = function (data) {
-    $http({method: "post", url: "/items", data: data})
-    .then(function(res){ 
-      get();
-    })
-  };
+        $scope.clear = function(){
+            $http({method: "delete", url: "/items"})
+                .then(function(res){
+                    get();
+                })
+        };
 
-  }]);
+        $scope.add = function(data){
+            $http({method: "post", url: "/items", data: data})
+                .then(function(res){
+                    get();
+                })
+        };
+
+    }]);
